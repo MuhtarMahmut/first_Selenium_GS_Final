9c9551cff463c09af523d6f0d30a9733912932fc
MuhtarMahmut <Muhtar.Mahmut0519@yahoo.com>
2019-02-20 16:28:30 -0500

    m
    m

--- src/test/java/Utilities/Mylibrary.java
+++ src/test/java/Utilities/Mylibrary.java
@@ -3,10 +3,16 @@ package Utilities;
 public class Mylibrary {
 
     public static void sleep(double a){
+
         try {
             Thread.sleep((int)(a*1000));
         } catch (InterruptedException e) {
 
         }
+
+        // github:
+        // 1. commit, commit and push. publish repository. update repository,   fork, merge,
+
+
     }
 }
